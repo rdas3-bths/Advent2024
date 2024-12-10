5dbcd6d28372fec479b6bbf62a248414cb28ede8
rdas3-bths <98552214+rdas3-bths@users.noreply.github.com>
2024-12-10 11:25:41 -0500

    Add files via upload

--- Day10.java
+++ Day10.java
@@ -1,12 +1,10 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Scanner;
 
 public class Day10 {
-
     public static void main(String[] args) {
         ArrayList<String> fileData = getFileData("src/InputFile");
         int rows = fileData.size();
@@ -15,33 +13,34 @@ public class Day10 {
         for (int r = 0; r < lavaMap.length; r++) {
             for (int c = 0; c < lavaMap[0].length; c++) {
                 String entry = fileData.get(r).substring(c, c + 1);
-                if (entry.equals(".")) lavaMap[r][c] = -100;
+                if (entry.equals("."))
+                    lavaMap[r][c] = -100;
                 else
                     lavaMap[r][c] = Integer.parseInt(entry);
-
             }
         }
 
         int partOneAnswer = 0;
         int partTwoAnswer = 0;
-
         for (int r = 0; r < lavaMap.length; r++) {
             for (int c = 0; c < lavaMap[0].length; c++) {
                 if (lavaMap[r][c] == 0) {
                     TrailHeadInfo trailHead = new TrailHeadInfo(r, c);
-                    traverseMap(lavaMap, r, c, trailHead);
+                    traverseMap(lavaMap, trailHead);
                     partOneAnswer += trailHead.getEndPoints().size();
                     partTwoAnswer += trailHead.getRating();
                 }
             }
         }
-
-
         System.out.println("Part one answer: " + partOneAnswer);
         System.out.println("Part two answer: " + partTwoAnswer);
     }
 
-    public static void traverseMap(int[][] lavaMap, int row, int col, TrailHeadInfo trailHead) {
+    public static void traverseMap(int[][] lavaMap, TrailHeadInfo trailHead) {
+
+        int row = trailHead.getTrackedRow();
+        int col = trailHead.getTrackedCol();
+
         // base case, found a 9
         if (lavaMap[row][col] == 9) {
             trailHead.addEndPoint(row + "," + col);
@@ -61,8 +60,11 @@ public class Day10 {
                 if (d == 'l') newCol--;
                 if (d == 'r') newCol++;
 
+                trailHead.setTrackedRow(newRow);
+                trailHead.setTrackedCol(newCol);
+
                 // recursive call to try each direction
-                traverseMap(lavaMap, newRow, newCol, trailHead);
+                traverseMap(lavaMap, trailHead);
             }
         }
     }
@@ -121,14 +123,14 @@ public class Day10 {
 class TrailHeadInfo {
     private HashSet<String> endPoints;
     private int rating;
-    private int startingRow;
-    private int startingCol;
+    private int trackedRow;
+    private int trackedCol;
 
     public TrailHeadInfo(int r, int c) {
         rating = 0;
         endPoints = new HashSet<String>();
-        startingRow = r;
-        startingCol = c;
+        trackedRow = r;
+        trackedCol = c;
     }
 
     public void addEndPoint(String endPoint) {
@@ -143,5 +145,21 @@ class TrailHeadInfo {
 
     public int getRating() {
         return rating;
+    }
+
+    public int getTrackedRow() {
+        return trackedRow;
+    }
+
+    public void setTrackedRow(int trackedRow) {
+        this.trackedRow = trackedRow;
+    }
+
+    public int getTrackedCol() {
+        return trackedCol;
+    }
+
+    public void setTrackedCol(int trackedCol) {
+        this.trackedCol = trackedCol;
     }
 }
