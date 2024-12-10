877e9d7a550077db37ea697bd3fac52678eb35ab
rdas3-bths <98552214+rdas3-bths@users.noreply.github.com>
2024-12-10 08:54:00 -0500

    Update and rename Day1.java to Day01.java

--- Day01.java
+++ Day01.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class Day1 {
+public class Day01 {
     public static void main(String[] args) {
 
         ArrayList<String> fileData = getFileData("src/InputFile");
