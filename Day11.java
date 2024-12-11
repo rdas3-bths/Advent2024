1ed1a6eca6ffb7c876701834230e56fd5084eade
rdas3-bths <98552214+rdas3-bths@users.noreply.github.com>
2024-12-11 10:55:59 -0500

    Add files via upload

--- Day11.java
+++ Day11.java
@@ -8,11 +8,14 @@ public class Day11 {
     public static void main(String[] args) {
         ArrayList<String> fileData = getFileData("src/InputFile");
         ArrayList<Long> numbers = new ArrayList<Long>();
+        HashMap<Long, Long> stoneMap = new HashMap<Long, Long>();
         String line = fileData.get(0);
         String[] stringNumbers = line.split(" ");
         for (String n : stringNumbers) {
             long stoneNumber = Long.parseLong(n);
             numbers.add(Long.parseLong(n));
+            long stoneCount = getStoneAmount(stoneMap, stoneNumber);
+            stoneMap.put(stoneNumber, stoneCount+1);
         }
 
 
@@ -29,8 +32,54 @@ public class Day11 {
 
 
         System.out.println("Part one answer: " + partOneAnswer);
+        int blinks = 75;
+        for (int i = 0; i < blinks; i++) {
+            doBlinkPartTwo(stoneMap);
+        }
+
+        long partTwoAnswer = 0;
+        for (long value : stoneMap.values()) {
+            partTwoAnswer += value;
+        }
+        System.out.println("Part two answer: " + partTwoAnswer);
 
 
+    }
+
+    public static void doBlinkPartTwo(HashMap<Long, Long> stoneMap) {
+        ArrayList<Long> keys = new ArrayList<Long>();
+        keys.addAll(stoneMap.keySet());
+        HashMap<Long, Long> stoneMapCopy = new HashMap<Long, Long>();
+        for (long key : keys) {
+            stoneMapCopy.put(key, getStoneAmount(stoneMap, key));
+        }
+        for (long key : keys) {
+            long stoneNumber = getStoneAmount(stoneMapCopy, key);
+            if (stoneNumber == 0)
+                continue;
+            String keyString = key + "";
+
+            if (key == 0) {
+                stoneMap.put((long)1, getStoneAmount(stoneMap,1) + stoneNumber);
+                stoneMap.put((long)0, getStoneAmount(stoneMap,0) - stoneNumber);
+            }
+
+            else if (keyString.length() % 2 == 0) {
+                String firstHalf = keyString.substring(0, keyString.length()/2);
+                String secondHalf = keyString.substring(keyString.length()/2);
+                long stoneLeft = Long.parseLong(firstHalf);
+                long stoneRight = Long.parseLong(secondHalf);
+                stoneMap.put(stoneLeft, getStoneAmount(stoneMap, stoneLeft) + stoneNumber);
+                stoneMap.put(stoneRight, getStoneAmount(stoneMap, stoneRight) + stoneNumber);
+                stoneMap.put(key, getStoneAmount(stoneMap, key) - stoneNumber);
+            }
+
+            else {
+                stoneMap.put(key * 2024, getStoneAmount(stoneMap, key * 2024) + stoneNumber);
+                stoneMap.put(key, getStoneAmount(stoneMap, key) - stoneNumber);
+            }
+
+        }
     }
 
     public static void doBlinkPartOne(ArrayList<Long> numbers) {
@@ -72,6 +121,15 @@ public class Day11 {
         }
         catch (FileNotFoundException e) {
             return fileData;
+        }
+    }
+
+    public static long getStoneAmount(HashMap<Long, Long> stones, long key) {
+        if (stones.get(key) == null) {
+            return (long)0;
+        }
+        else {
+            return stones.get(key);
         }
     }
 }
