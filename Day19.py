dc97d66604af895fc4ab699b8942434601c5da6e
rdas3-bths <98552214+rdas3-bths@users.noreply.github.com>
2024-12-19 08:14:52 -0500

    Add files via upload

--- Day19.py
+++ Day19.py
@@ -8,16 +8,25 @@ def get_file_data(file_name):
         data.append(line.rstrip())
     return data
 
-
+# we can use caching here because looking at the input shows that
+# we have MANY repeated patterns that should always result in the
+# same return value.
+# python makes this simple:
 @cache
 def is_possible(towel):
+    # base case, we have an empty string
+    # just added a counter for part 2 ezpz
     if towel == '':
         return True, 1
     possible = False
     count = 0
+    # start checking the beginning of each string
     for start in available_designs:
+        # if the towel starts with one of the available designs
         if towel.startswith(start):
+            # recursive call with the substring after the start design
             check, number = is_possible(towel[len(start):])
+            # update possible if we found a match
             if check:
                 possible = True
             count += number
